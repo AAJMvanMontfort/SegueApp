688c05519f578430d4dcb8b1d7bfc2d1256cb6a5
AAJMvanMontfort <lout@me.com>
2019-09-21 14:53:39 +0200

    First workable version and some functions added to see when things happen within Swift

--- SegueApp/ViewController.swift
+++ SegueApp/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var userName = ""
+
+    @IBOutlet weak var txtInput: UITextField!
+
+    @IBAction func btnNext(_ sender: Any) {
+        userName = txtInput.text!
+        
+        performSegue(withIdentifier: "FromVC1ToVC2", sender: nil)
+    
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("viewDidLoad 1 called")
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear 1 called")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear 1 called")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewWillDissappear 1 called")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewDidDissappear 1 called")
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "FromVC1ToVC2" {
+            let destinationVC = segue.destination as! SecondViewController
+            
+            destinationVC.myName = userName
+        }
+    }
+    
 }
 
